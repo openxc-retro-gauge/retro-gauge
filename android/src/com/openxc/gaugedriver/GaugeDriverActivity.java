81dd375df2f3e53c4e7144b84727f7cc0ea23d9d
David Melcher <dmelche6@ford.com>
2013-02-06 11:24:14 -0500

    Added a debug text view.  Fixed the incorrect "Sending Updates" indicator.

--- android/src/com/openxc/gaugedriver/GaugeDriverActivity.java
+++ android/src/com/openxc/gaugedriver/GaugeDriverActivity.java
@@ -48,6 +48,7 @@ public class GaugeDriverActivity extends Activity {
     private ToggleButton mToggleButton;
     private TextView mStatusText;
     private TextView mSendText;
+    private TextView mDebugText;
 
     private PendingIntent mPermissionIntent;
     UsbManager mUsbManager = null;
@@ -202,6 +203,7 @@ public class GaugeDriverActivity extends Activity {
         }
 
         mSendText = (TextView) findViewById(R.id.editTextManualData);
+        mDebugText = (TextView) findViewById(R.id.editTextDebug);
 
         mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
 
@@ -251,7 +253,7 @@ public class GaugeDriverActivity extends Activity {
                     mSerialStarted = mSerialPort.begin(9600);
                     if (mSerialStarted)
                     {
-                        if(mReceiveTimer == null)   //Start the updates.
+                        if(mReceiveTimer != null)   //Make sure we're not running updates.
                             onTimerToggle(null);
                     } else
                     {
@@ -285,6 +287,16 @@ public class GaugeDriverActivity extends Activity {
         runOnUiThread(new Runnable() {
             public void run() {
                 mStatusText.setText(outCS);
+            }
+        });
+    }
+    
+    private void UpdateDebug(String newMessage) {
+        final CharSequence outCS = newMessage;
+        runOnUiThread(new Runnable() {
+            public void run() {
+                //mDebugText.append(outCS);
+            	mDebugText.setText(outCS);
             }
         });
     }
@@ -378,6 +390,8 @@ public class GaugeDriverActivity extends Activity {
 
         writeStringToSerial("(" + String.format("%02d", value) + "|" +
                 String.format("%02d", iPercent) + ")");
+        //UpdateDebug("(" + String.format("%02d", value) + "|" +
+        //        String.format("%02d", iPercent) + ")");
     }
 
     private void writeStringToSerial(String outString){
