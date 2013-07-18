2b3c338d3274c07f8ee3c9a8d0157b58f1268668
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-07-18 14:59:34 -0400

    Remove unused imports.

--- android/src/com/openxc/gaugedriver/GaugeDriverActivity.java
+++ android/src/com/openxc/gaugedriver/GaugeDriverActivity.java
@@ -12,7 +12,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
-import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.os.IBinder;
